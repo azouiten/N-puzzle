53f9bcbc93b55f3e2a8d99f2f0dda810919ee7e8
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-05-24 19:09:42 +0100

    -

--- Parser.cpp
+++ Parser.cpp
@@ -7,14 +7,18 @@
 typedef std::vector<std::string >       t_content;
 typedef std::vector<std::vector<u_int> >  t_state;
 
+
 std::string trim(std::string str)
 {
-    int i;
+    size_t i, j;
 
     i = 0;
     while (i < str.size() && str[i] == ' ' && ++i);
-    return (str.substr(i, str.size() - i));
+    j = str.size() - 1;
+    while (j != i && str[j] == ' ' && j--);
+    return (str.substr(i, j - i + 1));
 }
+
 
 bool is_number(std::string str)
 {
@@ -25,24 +29,32 @@ bool is_number(std::string str)
     return (!str.empty() && it == str.end());
 }
 
-std::vector<std::string> read(std::string file_name)
+
+
+std::vector<std::string> read(std::string filename)
 {
     std::ifstream   ifs;
     t_content       content;
     std::string     line;
+    int pos;
 
-    ifs.open(file_name, std::ios::in);
+    ifs.open(filename, std::ios::in);
     if (ifs.is_open())
     {
         while (getline(ifs, line))
         {
             line = trim(line);
             if (line.size() && line[0] != '#')
-                content.push_back(trim(line));
+            {
+                if ((pos = line.find('#')) != std::string::npos)
+                    line = std::string(line, 0, pos);
+                content.push_back(line);
+            }
         }
     }
     return (content);
 }
+
 
 std::vector<std::string > split(std::string str, int del)
 {
@@ -69,23 +81,28 @@ std::vector<std::string > split(std::string str, int del)
     return (tokens);
 }
 
+
 int parse_content(t_content content, u_short &dim, t_state &state)
 {
     t_content::iterator         it;
     std::string                 line;
     std::vector<std::string >   tokens;
     std::vector<u_int >         row;
+    u_int                       total, dummy, count;
 
-    // If content is empty, return error flag (-1)
     if (content.size() == 0)
         return (-1);
 
-    // Parse the dimension line
     it = content.begin();
     try
     {
+        std::cout << *it << "|" << std::endl;
         if (!is_number(*it))
+        {
+        std::cout << *it << std::endl;
+
             throw std::invalid_argument("retard");
+        }
         dim = std::stoi(*it);
     }
     catch (std::invalid_argument)
@@ -93,39 +110,46 @@ int parse_content(t_content content, u_short &dim, t_state &state)
         return (-1);
     }
 
-    // Parse puzzle lines
+    total = 0;
+    dummy = 0;
+    count = 0;
     it++;
     while (it != content.end())
     {
         tokens = split(*it, ' ');
+        if (tokens.size() != dim)
+            return (-1);
         for (int i = 0; i < tokens.size(); i++)
         {
             try
             {
                 if (!is_number(tokens[i]))
                     return (-1);
-                row.push_back(stoi(row))
+                total += std::stoi(tokens[i]);
+                dummy += count++;
+                row.push_back(std::stoi(tokens[i]));
             }
             catch(const std::exception& e)
             {
-               return (-1);
+                return (-1);
             }
         }
+        state.push_back(row);
         row.clear();
         it++;
     }
-
+    if (total != dummy)
+        return (-1);
     return (0);
 }
 
-int main(int argc, char **argv)
-{
-    u_short dim;
-    t_state state;
 
-    if (parse_content(read(argv[1]), dim, state) == -1)
+t_state parse(std::string filename)
+{
+    t_state state;
+    u_short dim;
+
+    if (parse_content(read(filename), dim, state) == -1)
         std::cout << "Invalid input file" << std::endl;
-    else
-        std::cout << "Cool" << std::endl;
-    return (0);
+    return (state);
 }
